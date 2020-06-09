3db49096c9432611cd98a0dc985758811c5c3a24
Corey Gumbs <cgumbswebdev@gmail.com>
2020-06-09 18:52:16 -0400

    created stack linked list with len and push methods

--- stack/stack.py
+++ stack/stack.py
@@ -10,23 +10,69 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         #returns length
+#         return len(self.storage)
+
+#     def push(self, value):
+#         #appends value to end of list
+#         return self.storage.append(value)
+
+#     def pop(self):
+#         #check if list empty, return None if true
+#         if not self.storage:
+#             return None
+#         else:
+#             #pop the last item out of list and return value
+#             return self.storage.pop()
+
+
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value;
+        self.next_node = next_node
+
 class Stack:
     def __init__(self):
-        self.size = 0
-        self.storage = []
-
+        self.head = None
+        self. tail = None
+    
     def __len__(self):
-        #returns length
-        return len(self.storage)
-
+        count = 0
+        current_head = self.head
+        while(current_head):
+            count +=1
+            current_head = current_head.next_node 
+        return count
+        
     def push(self, value):
-        #appends value to end of list
-        return self.storage.append(value)
-
-    def pop(self):
-        #check if list empty, return None if true
-        if not self.storage:
-            return None
+        new_node = Node(value, None);
+        #check if head is empty
+        if not self.head:
+            self.head = new_node;
         else:
-            #pop the last item out of list and return value
-            return self.storage.pop()
+            head = self.head;
+            new_node.next_node = self.head
+            self.head = new_node
+            
+    def __str__(self):
+        return f"{self.head} {self.head.next_node}"
+    
+test_node =  Node(1, None);
+
+
+
+new_stack = Stack();
+
+new_stack.push(1);
+print(new_stack);
+new_stack.push(2)
+print(new_stack)
+new_stack.push(3)
+print(new_stack)
+print(new_stack.__len__())
